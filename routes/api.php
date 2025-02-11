0a3f0063ae63ab6030f681da55b51df17947c1ea
gastonmiguel <gastonmiguel@live.com.ar>
2025-02-11 00:36:42 -0300

    fix controller

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,13 @@
 <?php
 
 use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\ColorController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/colors', [ColorController::class, 'index']);
 
 Route::get('/companies', [CompanyController::class, 'index']);
 Route::get('/companies/{slug}', [CompanyController::class, 'show']);
